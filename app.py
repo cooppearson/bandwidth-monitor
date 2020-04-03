36eca4419ed53d77a84f18f3598c551d2a93d7f5
cooppearson <cooppearson@gmail.com>
2020-04-03 10:02:17 -0400

    Auto-indented app.py
    Added schedule & speedtest-cli to requirements.txt
    
    Added a gitignore file
    
    Auto-indented speed.py and added 10 minute schedule to speedtest
    
    Updated readme

--- app.py
+++ app.py
@@ -12,83 +12,83 @@ app = Flask(__name__)
 app.debug = True
 
 def scale(s):
-    """Scale bandwidths in Bytes to MB."""
-    if s > 100:
-        return s // 1000000
-    return s
+	"""Scale bandwidths in Bytes to MB."""
+	if s > 100:
+		return s // 1000000
+	return s
 
 @app.route('/')
 def index():
-    with sqlite3.connect('speedtest.db') as connection:
-        rows = connection.execute('SELECT * FROM RESULTS').fetchall()
+	with sqlite3.connect('speedtest.db') as connection:
+		rows = connection.execute('SELECT * FROM RESULTS').fetchall()
 
-        x_axis_fast = [
-            datetime.datetime.fromtimestamp(r[1]) for r in rows
-            if r[0] == 'fast.com'
-        ]
-        x_axis_speed = [
-            datetime.datetime.fromtimestamp(r[1]) for r in rows
-            if r[0] != 'fast.com'
-        ]
+		x_axis_fast = [
+			datetime.datetime.fromtimestamp(r[1]) for r in rows
+			if r[0] == 'fast.com'
+		]
+		x_axis_speed = [
+			datetime.datetime.fromtimestamp(r[1]) for r in rows
+			if r[0] != 'fast.com'
+		]
 
-        graph = dict(
-            data=[
-                go.Scatter(
-                    x = x_axis_fast,
-                    y = [
-                        scale(r[3]) for r in rows
-                        if r[0] == 'fast.com'
-                    ],
-                    mode = 'lines+markers',
-                    name = 'Download: fast.com'
-                ),
-                go.Scatter(
-                    x = x_axis_speed,
-                    y = [
-                        scale(r[3]) for r in rows
-                        if r[0] != 'fast.com'
-                    ],
-                    mode = 'lines+markers',
-                    name = 'Download: speedtest-cli'
-                ),
-                go.Scatter(
-                    x = x_axis_speed,
-                    y = [
-                        scale(r[4]) for r in rows
-                        if r[0] != 'fast.com'
-                    ],
-                    mode = 'lines+markers',
-                    name = 'Upload'
-                ),
-                go.Scatter(
-                    x = x_axis_speed,
-                    y = [
-                        r[2] for r in rows
-                        if r[0] != 'fast.com'
-                    ],
-                    mode = 'lines+markers',
-                    name = 'Ping'
-                )
-            ],
-            layout=dict(
-                title='Bandwidth',
-                yaxis=dict(
-                    title="Bandwidth"
-                ),
-                xaxis=dict(
-                    title="Time"
-                )
-            )
-        )
+		graph = dict(
+			data=[
+				go.Scatter(
+					x = x_axis_fast,
+					y = [
+						scale(r[3]) for r in rows
+						if r[0] == 'fast.com'
+					],
+					mode = 'lines+markers',
+					name = 'Download: fast.com'
+				),
+				go.Scatter(
+					x = x_axis_speed,
+					y = [
+						scale(r[3]) for r in rows
+						if r[0] != 'fast.com'
+					],
+					mode = 'lines+markers',
+					name = 'Download: speedtest-cli'
+				),
+				go.Scatter(
+					x = x_axis_speed,
+					y = [
+						scale(r[4]) for r in rows
+						if r[0] != 'fast.com'
+					],
+					mode = 'lines+markers',
+					name = 'Upload'
+				),
+				go.Scatter(
+					x = x_axis_speed,
+					y = [
+						r[2] for r in rows
+						if r[0] != 'fast.com'
+					],
+					mode = 'lines+markers',
+					name = 'Ping'
+				)
+			],
+			layout=dict(
+				title='Bandwidth',
+				yaxis=dict(
+					title="Bandwidth"
+				),
+				xaxis=dict(
+					title="Time"
+				)
+			)
+		)
 
-        return render_template(
-            'layouts/index.html',
-            graph=json.dumps(
-                graph,
-                cls=plotly.utils.PlotlyJSONEncoder
-            ),
-        )
+		return render_template(
+			'layouts/index.html',
+			graph=json.dumps(
+				graph,
+				cls=plotly.utils.PlotlyJSONEncoder
+			),
+		)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9999)
+	app.run(host='0.0.0.0', port=9999)
